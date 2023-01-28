f228039f9b3460e7a2b34e4f0819da8df346c58c
Arjun Ghimire <82378187+Arjun-Ghimire@users.noreply.github.com>
2023-01-28 15:03:36 +0545

    first java code

---
+++ helloworld.java
@@ -0,0 +1,13 @@
+// This below line is actually not needed, by default compiler includes java.lang to the code.
+import java.lang.*;
+
+class helloworld{
+
+
+public static void main(String[] args){
+    System.out.println("Okay, this is my first java code!");
+    System.out.print("pushing it to the github.");
+
+}
+
+}
